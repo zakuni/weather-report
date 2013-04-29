c19c4973a960054ad111719e25f789b9c600e5da
zakuni <kunio038@gmail.com>
2013-04-29 21:52:33 +0900

    add attr today, tomorrow, day_after_tomorrow

--- lib/weather-report.rb
+++ lib/weather-report.rb
@@ -1,15 +1,40 @@
+# -*- coding: utf-8 -*-
 require 'json'
 require 'open-uri'
 require "weather-report/version"
 
 module WeatherReport
   class Weather
+    attr_reader :today, :tomorrow, :day_after_tomorrow
+
     def initialize(city)
       @uri = URI.parse("http://weather.livedoor.com/forecast/webservice/json/v1?city=#{city}")
     end
 
-    def report
-      JSON.parse(@uri.read)
+    def today
+      @today ||= forecast("今日")
+    end
+    
+    def tomorrow
+      @tomorrow ||= forecast("明日")
+    end
+
+    def day_after_tomorrow
+      @day_after_tomorrow ||= forecast("明後日")
+    end
+
+    private
+
+    def forecast(dateLabel)
+      forecasts.each {|elem| return elem if elem["dateLabel"] == dateLabel}
+    end
+    
+    def forecasts
+      @forecasts ||= read["forecasts"]
+    end
+
+    def read
+      @response ||= JSON.parse(@uri.read)
     end
   end
 end
--- test/test_weather-report.rb
+++ test/test_weather-report.rb
@@ -5,10 +5,27 @@ require 'test_helper'
 class TestWeatherReport < MiniTest::Unit::TestCase
   include WeatherReport
 
+  def setup
+    # 130010は東京
+    @weather = Weather.new(130010)
+  end
+
   def test_initialize
     assert_raises ArgumentError do
       Weather.new
     end
     assert_instance_of Weather, Weather.new(130010)
+  end
+
+  def test_today
+    assert_respond_to @weather, :today
+  end
+
+  def test_tomorrow
+    assert_respond_to @weather, :tomorrow
+  end
+
+  def test_day_after_tomorrow
+    assert_respond_to @weather, :day_after_tomorrow
   end
 end
