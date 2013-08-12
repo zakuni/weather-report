762831a287d5f1bbfa8de9dde082152da470b20e
zakuni <kunio038@gmail.com>
2013-08-13 00:09:29 +0900

    modify request_cityid to raise ArgumentError with invalid argument.

--- lib/weather-report/weather.rb
+++ lib/weather-report/weather.rb
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
 module WeatherReport
-  def self.cities
-    proxy = Weather.parse_proxy(ENV["http_proxy"])
-    doc = Nokogiri::XML(open("http://weather.livedoor.com/forecast/rss/primary_area.xml", :proxy_http_basic_authentication => [proxy.server, proxy.user, proxy.pass]))
-    doc.xpath("//city").map{|i|
-      i["title"]
-    }
-  end
-
   class Weather
     attr_reader :today, :tomorrow, :day_after_tomorrow
 
@@ -18,13 +10,12 @@ module WeatherReport
 
     # @return [String] the id of given city
     def self.request_cityid(city_name)
+      raise ArgumentError, "City name must be String." unless city_name.kind_of?(String)
       proxy = Weather.parse_proxy(ENV["http_proxy"])
       doc = Nokogiri::XML(open("http://weather.livedoor.com/forecast/rss/primary_area.xml", :proxy_http_basic_authentication => [proxy.server, proxy.user, proxy.pass]))
       doc.search("//city[@title='#{city_name}']").attr("id").value
     rescue NoMethodError
       raise WeatherReportError, "It seems like city #{city_name} does not exist.\nPlease look at http://weather.livedoor.com/forecast/rss/primary_area.xml for city list."
-    rescue => e
-      raise WeatherReportError
     end
 
     def self.parse_proxy(proxy)
