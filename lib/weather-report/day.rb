b288e55c0680f9c5af40d99e750cf35cbd3bda31
zakuni <kunio038@gmail.com>
2020-09-15 21:40:13 +0900

    refactor
    initialize instance variables in constructor, which both attr_reader and method was defined

--- lib/weather-report/day.rb
+++ lib/weather-report/day.rb
@@ -6,7 +6,15 @@ module WeatherReport
 
     def initialize(forecasts, dateLabel)
       raise ArgumentError, "dateLabel must be 今日, 明日 or 明後日" unless dateLabel =~ /(今日|明日|明後日)/
+
       @forecast = forecast(forecasts, dateLabel)
+      @telop = @forecast['telop']
+      year, month, day = @forecast['date'].split('-')
+      @date = Date.new(year.to_i, month.to_i, day.to_i)
+      min = @forecast["temperature"]["min"]
+      @temperature_min = min ? min["celsius"].to_i : nil
+      max = @forecast["temperature"]["max"]
+      @temperature_max = max ? max["celsius"].to_i : nil
     end
 
     # @return [Boolean] return true if it rains.
@@ -22,33 +30,6 @@ module WeatherReport
     # @return [Boolean] return true if it will be rainy or snowy or sleety or hailstorm
     def umbrella?
       telop =~ /[雨雪霙雹]/ ? true : false
-    end
-
-    # @return [Date] the date
-    def date
-      year, month, day = @forecast["date"].split('-')
-      @date ||= Date.new(year.to_i, month.to_i, day.to_i)
-    end
-
-    # @return [String] the telop
-    def telop
-      @telop ||= @forecast["telop"]
-    end
-
-    # @return [Fixnum] the minimum temperature.
-    # Temperature of today could be nil.
-    def temperature_min
-      min = @forecast["temperature"]["min"]
-      @temperature ||=
-        min ? min["celsius"].to_i : nil
-    end
-
-    # @return [Fixnum] the maximum temperature.
-    # Temperature of today could be nil.
-    def temperature_max
-      max = @forecast["temperature"]["max"]
-      @temperature_max ||=
-        max ? max["celsius"].to_i : nil
     end
 
     # @return [Hash] return with hash format.
